6f9270d3064ec2f2f5cfeb0aab32c2af195036f3
Conrad Hughes <chughes@learningobjects.com>
2015-08-22 22:24:33 +0100

    Documentation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+# A pair of functions to wrap a matrix with a facility to cache its inverse.
+# Example usage:
 
-## Write a short comment describing this function
+#   orig = matrix(rnorm(25),nrow=5,ncol=5)  # Create a random 5x5 matrix
+#   cmatrix = makeCacheMatrix(orig)         # Wrap with inverse cache
+#   inverse = cacheSolve(cmatrix)           # Get the inverse
+#   cmatrix$get() %*% inverse               # Check result
+#   i2 = cacheSolve(cmatrix)                # Should say "getting cached data"
+
+# makeCacheMatrix() creates a list of get/set/getinverse/setinverse functions
+# that operate identically to the makeVector example given in the assignment:
+# getting and setting a matrix value and its cached inverse value,
+# respectively.  Setting the matrix value clears any cached inverse value.  The
+# functions execute in a closure containing the original matrix and (if
+# present) its cached inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -18,7 +29,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# As with the cachemean example, here we calculate and return a matrix's
+# inverse; if we calculated it before, then we return the remembered inverse
+# instead of recalculating.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
